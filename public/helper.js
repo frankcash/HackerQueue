62d1f1e029ca7ab61222103c671115370013b43a
frankcash <cfcash@mtu.edu>
2014-05-24 15:57:50 -0400

    makes things an unordered list.

--- public/helper.js
+++ public/helper.js
@@ -2,21 +2,21 @@
 $.getJSON('/ycomb', function(data){ // gets the JSON info
   for(var i = 0; i<30;i++){
   	console.log("ycom");
-    $("<p>"  +"<a href=" + data[i].url + ">" + data[i].title + "</a></p>").appendTo("#helper")
+    $("<li><p>"  +"<a href=" + data[i].url + ">" + data[i].title + "</a></p></li>").appendTo("#helper")
   }
 });
 
 $.getJSON('/rp', function(data){
 	for(var i=0; i<25; i++){
 		console.log("rp");
-		$("<p>"+ "<a href=" + data[i].uri + ">" +data[i].title+ "</a>"+"</p>").appendTo("#helper")
+		$("<li><p>"+ "<a href=" + data[i].uri + ">" +data[i].title+ "</a>"+"</p></li>").appendTo("#helper")
 	}
 });
 
 $.getJSON('/lobster', function(data){
 	for (var i = 0; i<25; i++) {
 		console.log("lobster");
-		$("<p><a href=" + data[i].url + ">" + data[i].title + "</a></p>").appendTo("#helper")
+		$("<li><p><a href=" + data[i].url + ">" + data[i].title + "</a></p></li>").appendTo("#helper")
   }
 });	
 
