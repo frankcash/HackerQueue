ef83d49c864d269d658ecf33897743d4c34245cf
frankcash <cfcash@mtu.edu>
2014-05-24 15:20:51 -0400

    helper.js support for reddit

--- public/helper.js
+++ public/helper.js
@@ -1,9 +1,18 @@
 $.getJSON('/ycomb', function(data){ // gets the JSON info
-  // console.log(data);
+   console.log("ycomb");
   for(var i = 0; i<30;i++){
-    $("<p><b>" + data[i].rank + ".</b> " + "<a href=" + data[i].url + ">" + data[i].title + "</a></p>").appendTo("#helper")
+  	console.log(data[i].url);
+    $("<p>"  +"<a href=" + data[i].url + ">" + data[i].title + "</a></p>").appendTo("#helper")
   }
 });
+
+$.getJSON('/rp', function(data){
+	console.log("rp");
+	for(var i=0; i<25; i++){
+		$("<p>"+ "<a href=" + data[i].uri + ">" +data[i].title+ "</a>"+"</p>").appendTo("#helper")
+	}
+});
+
 $( document ).ready(function() {
   // console.log( "ready!" );
   // $("b").mouseover(function(){
