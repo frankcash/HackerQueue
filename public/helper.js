6f181102e21e74a2d8ceaf48256c988670a007f0
frankcash <cfcash@mtu.edu>
2014-05-24 21:10:46 -0400

    (Fixes #3)(Close #3) adds instant search for filter

--- public/helper.js
+++ public/helper.js
@@ -1,22 +1,18 @@
-
-$.getJSON('/ycomb', function(data){ // gets the JSON info
-  for(var i = 0; i<30;i++){
-  	console.log("ycom");
-    $('<li><p><a href="' + data[i].url + '">' + data[i].title + '</a></p></li>').appendTo('#helper');
+$.getJSON('/ycomb', function(data){ 
+  for(var i = 0; i<10;i++){
+    $('<li id="post"><p><a href="' + data[i].url + '">' + data[i].title + '</a></p></li>').appendTo('#helper');
   }
 });
 
 $.getJSON('/rp', function(data){
-	for(var i=0; i<25; i++){
-		console.log("rp");
-		$('<li><p><a href="' + data[i].url + '">' + data[i].title + '</a></p></li>').appendTo('#helper');
+	for(var i=0; i<10; i++){
+		$('<li id="post"><p><a href="' + data[i].url + '">' + data[i].title + '</a></p></li>').appendTo('#helper');
 	}
 });
 
 $.getJSON('/lobster', function(data){
-	for (var i = 0; i<25; i++) {
-		console.log("lobster");
-		$('<li><p><a href="' + data[i].url + '">' + data[i].title + '</a></p></li>').appendTo('#helper');
+	for (var i = 0; i<10; i++) {
+		$('<li id="post"><p><a href="' + data[i].url + '">' + data[i].title + '</a></p></li>').appendTo('#helper');
   }
 });	
 
@@ -25,4 +21,54 @@ $( document ).ready(function() {
   $('#refresh').click(function(){
     location.reload();
   });
+
+
+
+	$("#searchterm").keyup(function(e){
+		var q = $("#searchterm").val();
+		$('li').remove();
+		$("#res").remove();
+		$("#helper").append("<b id=res> Results for " + q + "</b>");
+
+		$.getJSON("/lobster", function(data){ 
+			
+			for(var i = 0; i<10; i++){
+				var re = new RegExp(q, "g");
+				var treble = re.test(data[i].title);
+				if(treble == true){
+					$('<li id="post"><p><a href="' + data[i].url + '">' + data[i].title + '</a></p></li>').appendTo('#helper');
+					console.log("lob");
+				}
+			}
+		})
+
+		$.getJSON("/rp", function(data){
+			// $('li').remove();
+			for(var i = 0; i<10; i++){
+				var re = new RegExp(q, "g");
+				var treble = re.test(data[i].title);
+				if(treble == true){
+					$('<li id="post"><p><a href="' + data[i].url + '">' + data[i].title + '</a></p></li>').appendTo('#helper');
+					console.log("rp");
+				}
+			}
+		})
+
+		$.getJSON("/ycomb", function(data){
+			// $('li').remove();
+			for(var i = 0; i<10; i++){
+				var re = new RegExp(q, "g");
+				var treble = re.test(data[i].title);
+				if(treble == true){
+					$('<li id="post"><p><a href="' + data[i].url + '">' + data[i].title + '</a></p></li>').appendTo('#helper');
+					console.log("yc");
+				}
+			}
+		})
+
+	})
+
+
+
+
 });
