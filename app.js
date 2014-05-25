3225550aab78db508befaaab0f5c5ce724c9d1fe
RoryStraubel <rstraubel@control-tec.com>
2014-05-24 23:05:37 -0400

    Fixed issue of Titles sometimes coming from description tags. Example: Scribd

--- app.js
+++ app.js
@@ -24,23 +24,15 @@ function YComb(callback){
   		if(!error && response.statusCode === 200){
         var metadataArray = [ ];
   			var $ = cheerio.load(html);
-  			$('span.comhead').each(function(i, element){
-  			var a=$(this).prev(); //selects previous data
-  			var rank=a.parent().parent().text(); //gets ranks by parsing text two elements higher
+  			$('span.title').each(function(i, element){
+  			var a=$(this); //selects previous data
   			var title=a.text(); // parses link title
-  			var url=a.attr('href'); // parses href attribute from "a" element
-  			var subtext = a.parent().parent().next().children('.subtext').children(); // gets the subtext from the children
-  			var points = $(subtext).eq(0).text();
-  			var username = $(subtext).eq(1).text();
-  			var comments = $(subtext).eq(2).text();
-
+  			var url=a.children.attr('href'); // parses href attribute from "a" element
+  		
   			var metadata = { // creates a new object
   				rank: parseInt(rank),
   				title:title,
-  				url:url,
-  				points: parseInt(points),
-  				username: username,
-  				comments: parseInt(comments)
+  				url:url
   			};
   			metadataArray.push(metadata); // pushes the object
   			});
