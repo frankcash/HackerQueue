0181f385522b5ff492c8803730bd174f4095daed
Frank Cash <cfcash@mtu.edu>
2014-08-02 22:09:08 -0400

    (#29) Refresh button
    Adds a ng-click and function for the refresh button.

--- public/test.js
+++ public/test.js
@@ -31,6 +31,7 @@ app.controller("AppTest", function($scope, $http){
   **/
   $http.get('/ynew').success(function(data) {
       $scope.hackerNew = data;
+      console.log(data);
   });
   $http.get('/lnew').success(function(data) {
       $scope.lNew = data;
@@ -38,5 +39,31 @@ app.controller("AppTest", function($scope, $http){
   $http.get('/rnew').success(function(data) {
       $scope.rNew = data;
   });
+
+  $scope.refresh = function(){
+    $http.get('/ycomb').success(function(data) {
+        $scope.hackerTop = data;
+        console.log(data);
+    });
+    $http.get('/lobster').success(function(data) {
+        $scope.lobTop = data;
+    });
+    $http.get('/rp').success(function(data) {
+        $scope.rTop = data;
+    });
+
+    /**
+    For new posts
+    **/
+    $http.get('/ynew').success(function(data) {
+        $scope.hackerNew = data;
+    });
+    $http.get('/lnew').success(function(data) {
+        $scope.lNew = data;
+    });
+    $http.get('/rnew').success(function(data) {
+        $scope.rNew = data;
+    });
+  }
 
 });
