0f8d87a47ac66e7d1b5ae8433700357d411c8521
frankcash <cashc@acm.org>
2015-03-26 16:29:19 -0400

    removes comment from gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,11 @@
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
-    // This line makes your node configurations available for use
     pkg: grunt.file.readJSON('package.json'),
-    // This is where we configure JSHint
     jshint: {
-      // You get to make the name
-      // The paths tell JSHint which files to validate
       options: { jshintrc: '.jshintrc' },
       myFiles: ['routes/*.js', "public/app.js" , 'app.js']
     }
   });
-  // Each plugin must be loaded following this pattern
   grunt.loadNpmTasks('grunt-contrib-jshint');
-
   grunt.registerTask('test', ['jshint']);
-
 };
