cd98f71ee0f334d14a242a39903510ed693267ad
Keari Eggers <kme211@users.noreply.github.com>
2016-10-10 18:40:01 -0500

    Fixes HackerNews parsing (#51)
    Fixes #45.

--- routes/hackernews.js
+++ routes/hackernews.js
@@ -4,32 +4,32 @@ var cheerio = require('cheerio');
 function parse(html){
   var metadataArray = [ ];
   var $ = cheerio.load(html);
-  $('span.comhead').each(function(){
-    var a=$(this).prev(); //selects previous data
-    var rank=a.parent().parent().text(); //gets ranks by parsing text two elements higher
-    var title=a.text(); // parses link title
-    var url=a.attr('href'); // parses href attribute from "a" element
-    var subtext = a.parent().parent().next().children('.subtext').children(); // gets the subtext from the children
-    var points = $(subtext).eq(0).text();
-    var username = $(subtext).eq(1).text();
+  $('.athing').each(function(){
+    var $storylink = $(this).find('.storylink');
+    var rank = $(this).find('.rank').text();
+    var title = $storylink.text();
+    var url = $storylink.attr('href');
+    var $subtext = $(this).next();
+    var points = $subtext.find('.score').text();
+    var username = $subtext.find('.hnuser').text();
+    var $comments = $subtext.find('a').last();
+    var comments = $comments.text();
     var YCOMB_COMMENT_URL = "https://news.ycombinator.com/";
-    var comments = $(subtext).eq(2).text();
-    var comments_link = YCOMB_COMMENT_URL + $(subtext).eq(2).attr('href');
+    var comments_link = YCOMB_COMMENT_URL + $comments.attr('href');
 
     var metadata = { // creates a new object
       rank: parseInt(rank),
       site: "HN",
-      title:title,
-      url:url,
+      title: title,
+      url: url,
       points: parseInt(points),
       username: username,
       comments: parseInt(comments),
-      comments_link:comments_link
+      comments_link: comments_link
     };
     metadataArray.push(metadata); // pushes the object
   });
-  return(metadataArray);
-  // res.send(metadataArray);
+  return metadataArray;
 }
 
 exports.htop = function(req,res){
