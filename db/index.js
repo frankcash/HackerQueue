de51b0c73d8484173f3f6f2248c22fa1a365a9f3
Frank Cash <cashc@acm.org>
2018-01-27 13:53:03 -0500

    trying to get hecking heroku to work

--- db/index.js
+++ db/index.js
@@ -1,6 +1,13 @@
 const { Pool } = require('pg')
 
-const pool = new Pool()
+// const pool = new Pool()
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
 
 module.exports = {
   query: (text, params, callback) => {
