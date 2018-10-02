a099b539841a33069ce388a62f9a4f4cc346f93e
Logan Arnett <logan@loganarnett.com>
2018-10-02 12:55:55 -0400

    updating from PR review

--- public/fuzzySearch.js
+++ public/fuzzySearch.js
@@ -1,9 +1,8 @@
 'use strict';
 
 function fuzzySearchFilterFactory() {
-  var fuzzySearch;
+  let fuzzySearch;
   return function (input, pattern, dataKey, createSearch) {
-    console.log(input)
     if (createSearch && input && input.length) {
       fuzzySearch = new FuzzySearch(input, dataKey);
     }
@@ -20,14 +19,9 @@ angular.module("app").filter('fuzzy', fuzzySearchFilterFactory);
 function FuzzySearch(items, dataKey) {
   this.items = items;
   this.length = items.length;
-  this.lowerCasedItems = [];
-  for (var i = 0; i < this.length; i++) {
-    if (dataKey) {
-      this.lowerCasedItems.push(items[i][dataKey].toLowerCase());
-    } else {
-      this.lowerCasedItems.push(items[i].toLowerCase());
-    }
-  }
+  this.lowerCasedItems = items.map(item => {
+    return dataKey ? item[dataKey].toLowerCase() : item.toLowerCase();
+  });
   this.index = this.createIndex();
   this.index2 = this.createIndex2();
   this.dumpIndex();
@@ -35,19 +29,18 @@ function FuzzySearch(items, dataKey) {
 }
 
 FuzzySearch.prototype.createIndex = function () {
-  var items = this.lowerCasedItems;
-  var length = this.length;
-  var i = 0, j = 0;
-  var index = {};
-  var word;
-  var wordLength;
-  var uniqueLettersInWord;
-  var letter;
-  for (i = 0; i < length; i++) {
+  const items = this.lowerCasedItems;
+  const length = this.length;
+  let index = {};
+  let word;
+  let wordLength;
+  let uniqueLettersInWord;
+  let letter;
+  for (let i = 0; i < length; i++) {
     word = items[i];
     wordLength = word.length;
     uniqueLettersInWord = {};
-    for (j = 0; j < wordLength; j++) {
+    for (let j = 0; j < wordLength; j++) {
       letter = word[j];
       if (!uniqueLettersInWord[letter]) {
         uniqueLettersInWord[letter] = true;
@@ -63,25 +56,22 @@ FuzzySearch.prototype.createIndex = function () {
 };
 
 FuzzySearch.prototype.createIndex2 = function () {
-  var items = this.lowerCasedItems;
-  var length = this.length;
-  var i = 0, j = 0;
-  var index = {};
-  var word = "";
-  var wordLength = 0;
-  var uniqueLettersInWord = {};
-  var uniqueLettersInRestOfWord = {};
-  var letter = "";
-  var index2 = {};
-  var k = 0;
-  var restOfWord = "";
-  var restOfWordLength = 0;
-  var j = 0;
-  for (i = 0; i < length; i++) {
+  const items = this.lowerCasedItems;
+  const length = this.length;
+  let index = {};
+  let word = "";
+  let wordLength = 0;
+  let uniqueLettersInWord = {};
+  let uniqueLettersInRestOfWord = {};
+  let letter = "";
+  let index2 = {};
+  let restOfWord = "";
+  let restOfWordLength = 0;
+  for (let i = 0; i < length; i++) {
     word = items[i];
     wordLength = word.length;
     uniqueLettersInWord = {};
-    for (j = 0; j < wordLength - 1; j++) {
+    for (let j = 0; j < wordLength - 1; j++) {
       letter = word[j];
       if (!uniqueLettersInWord[letter]) {
         uniqueLettersInWord[letter] = true;
@@ -93,7 +83,7 @@ FuzzySearch.prototype.createIndex2 = function () {
           index2 = index[letter];
         }
         uniqueLettersInRestOfWord = {};
-        for (k = 0; k < restOfWordLength; k++) {
+        for (let k = 0; k < restOfWordLength; k++) {
           letter = restOfWord[k];
           if (!uniqueLettersInRestOfWord[letter]) {
             uniqueLettersInRestOfWord[letter] = true;
@@ -111,25 +101,25 @@ FuzzySearch.prototype.createIndex2 = function () {
 };
 
 FuzzySearch.prototype.dumpIndex = function () {
-  var index = this.index;
-  var humanReadableIndex = {};
-  for (var key in index) {
+  const index = this.index;
+  let humanReadableIndex = {};
+  for (let key in index) {
     humanReadableIndex[key] = [];
-    for (var i = 0; i < index[key].length; i++) {
+    for (let i = 0; i < index[key].length; i++) {
       humanReadableIndex[key].push(this.items[index[key][i]]);
     }
   }
 };
 
 FuzzySearch.prototype.dumpIndex2 = function () {
-  var index = this.index2;
-  var twoLetters;
-  var humanReadableIndex = {};
-  for (var key1 in index) {
-    for (var key2 in index[key1]) {
+  const index = this.index2;
+  let twoLetters;
+  let humanReadableIndex = {};
+  for (let key1 in index) {
+    for (let key2 in index[key1]) {
       twoLetters = "'" + key1 + key2 + "'";
       humanReadableIndex[twoLetters] = [];
-      for (var i = 0; i < index[key1][key2].length; i++) {
+      for (let i = 0; i < index[key1][key2].length; i++) {
         humanReadableIndex[twoLetters].push(this.items[index[key1][key2][i]]);
       }
     }
@@ -137,29 +127,28 @@ FuzzySearch.prototype.dumpIndex2 = function () {
 };
 
 FuzzySearch.prototype.match = function (string, pattern) {
-  var i = 0;
-  var j = 0;
-  var stringLen = string.length;
-  var patternLen = pattern.length;
-  for (i = 0, j = 0; i < patternLen && j < stringLen; j++) {
+  const stringLen = string.length;
+  const patternLen = pattern.length;
+  let matchLength = 0
+  for (let i = 0, j = 0; i < patternLen && j < stringLen; j++) {
     if (pattern[i] === string[j]) {
       i++;
+      matchLength++;
     }
   }
-  return i === patternLen;
+  return matchLength === patternLen;
 };
 
 FuzzySearch.prototype.search = function (pattern) {
   pattern = pattern.toLowerCase();
-  var results = [];
-  var lowercasedItems = this.lowerCasedItems;
-  var patternLen = pattern.length;
-  var wordIndexes = (patternLen > 1 ? this.index2[pattern[0]][pattern[1]] : this.index[pattern[0]]) || [];
-  var wordIndexlength = wordIndexes.length;
-  var items = this.items;
-  var i = 0;
-  var wordIndex;
-  for (i = 0; i < wordIndexlength; i++) {
+  const lowercasedItems = this.lowerCasedItems;
+  const items = this.items;
+  const patternLen = pattern.length;
+  const wordIndexes = (patternLen > 1 ? this.index2[pattern[0]][pattern[1]] : this.index[pattern[0]]) || [];
+  const wordIndexlength = wordIndexes.length;
+  let results = [];
+  let wordIndex;
+  for (let i = 0; i < wordIndexlength; i++) {
     wordIndex = wordIndexes[i];
     if (this.match(lowercasedItems[wordIndex], pattern)) {
       results.push(items[wordIndex]);
